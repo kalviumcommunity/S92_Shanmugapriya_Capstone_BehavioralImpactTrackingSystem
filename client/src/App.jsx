76f0c91e45713092185c15c67ba719e4c5ef9a57
shanmugat7ystdc867dc <shanmugapriya4563@gmail.com>
2026-09-05 12:22:39 +0530

    Implement update and delete functionality in React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import BehaviorManager from "./components/BehaviorManager";
 import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <Navbar />
-      <Home />
-      <Footer />
-    </div>
-  );
+return ( <div className="app"> <Navbar /> <Home /> <BehaviorManager /> <Footer /> </div>
+);
 }
 
 export default App;
